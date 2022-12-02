936def24c978444259fa5f7cef789b8da1b2c74d
Usshel <pawelopadczuk@gmail.com>
2022-12-02 15:45:46 +0100

    feat: sorting by price added to filtered list

--- src/app/components/filtered-product-list-second/filtered-product-list-second.component.ts
+++ src/app/components/filtered-product-list-second/filtered-product-list-second.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
-import { Observable, Subject, combineLatest } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
+import {BehaviorSubject, Observable, Subject, combineLatest, of, switchMap} from 'rxjs';
+import { map } from 'rxjs/operators';
 import { ProductModel } from '../../models/product.model';
 import { CategoriesService } from '../../services/categories.service';
 import { ProductService } from '../../services/product.service';
@@ -14,20 +14,41 @@ import { ProductService } from '../../services/product.service';
 export class FilteredProductListSecondComponent {
   readonly categories$: Observable<string[]> = this._categoriesService.getAll();
   private _categorySubject: Subject<string> = new Subject<string>();
+  private _orderSubject: BehaviorSubject<string> = new BehaviorSubject<string>('asc');
+
   public category$: Observable<string> = this._categorySubject.asObservable();
+  public order$: Observable<string> = this._orderSubject.asObservable();
+
+
   readonly products$: Observable<ProductModel[]> = combineLatest([
-    this._productService.getAll(),
-    this.category$
+    this.category$,
+    this.order$
   ]).pipe(
-    map(([products, category]: [ProductModel[], string]) => {
-      return products.filter(product => product.category === category);
-    })
+    switchMap(([category, order]: [string, string]) => this._productService.getAll().pipe(
+
+      map((products) => products.filter((product) => product.category === category)),
+
+      map((products) => {
+        return products.sort((a, b) => {
+          if (a.price > b.price) return order === 'asc' ? 1 : -1;
+          if (a.price < b.price) return order === 'asc' ? -1 : 1;
+          return 0;
+        })
+      }))
+    )
   );
+
+
+  public orders: Observable<string[]> = of(['asc','desc'])
 
   constructor(private _categoriesService: CategoriesService, private _productService: ProductService) {
   }
 
   selectCategory(category: string): void {
     this._categorySubject.next(category);
+  }
+
+  sort(order: string): void {
+    this._orderSubject.next(order);
   }
 }
